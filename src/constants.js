67cd23c7e3c67ec644e3ec88190bf04613a2cba5
Baasankhuu Software <88529575+baasankhuu-p@users.noreply.github.com>
2023-05-01 17:24:16 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const Backcolor = "";
 export const Maincolor = "";
 export const ActiveTxtcolor = "";
 export const Txtcolor = "";
-export const RestApiUrl = "http://localhost:3001";
+export const RestApiUrl = "https://simple-sharing-api.herokuapp.com";
